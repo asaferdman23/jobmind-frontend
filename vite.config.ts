7403cc26d282f50de4637c08ecb38b1631554caf
Asaf erdman <asaferdman23@gmail.com>
2026-03-21 21:52:57 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 5173,
     host: "127.0.0.1",
